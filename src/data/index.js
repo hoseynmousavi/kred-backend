8014cce7ad316563dab657800d100cee88731dbf
Hoseyn Mousavi <hoseyn.mousavi78@gmail.com>
2019-12-04 00:11:39 +0330

    fix

--- src/data/index.js
+++ src/data/index.js
@@ -2,7 +2,7 @@ const domain_url = "https://www.kred.ir/"
 const restful_url = "https://restful.kred.ir/"
 const port = 1435
 const sign = "0x410x620x650x6c"
-const connectServerDb = 'mongodb://seyed_hoseyn_kred:HHH123456hhh@188.212.22.166:27017/kred'
+const connectServerDb = "mongodb://seyed_hoseyn_kred:HHH123456hhh@188.212.22.166:27017/kred"
 const connectLocalDb = "mongodb://seyed_hoseyn:HH123456hh@localhost:27017/kred"
 
 const data = {
