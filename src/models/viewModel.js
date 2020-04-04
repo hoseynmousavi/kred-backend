b5514c72a976e7738c995f22e0dd25a0556daed6
Hoseyn Mousavi <hoseyn.mousavi78@gmail.com>
2020-04-04 18:32:40 +0430

    fix

--- src/models/viewModel.js
+++ src/models/viewModel.js
@@ -6,15 +6,18 @@ const viewModel = new schema({
     user_id: {
         type: schema.Types.ObjectId,
         ref: "user",
+        index: true,
     },
     type: {
         type: String,
         enum: ["page", "video"],
         required: "Enter type!",
+        index: true,
     },
     content: {
         type: String,
         required: "Enter content!",
+        index: true,
     },
     content_id: {
         type: schema.Types.ObjectId,
